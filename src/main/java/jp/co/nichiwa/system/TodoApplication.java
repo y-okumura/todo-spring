e93d50d5137874dfb0d645934d715350a19e595c
Y Okumura <y-okumura@nichiwa-system.co.jp>
2016-10-31 18:29:16 +0900

    start.spring.ioでプロジェクト作成
    Web, mybatis, hsqldb, thymeleafを選択

---
+++ src/main/java/jp/co/nichiwa/system/TodoApplication.java
@@ -0,0 +1,12 @@
+package jp.co.nichiwa.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+}
